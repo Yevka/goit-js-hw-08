1d237e6e5d56d05d7e8b0c3aa7ca5fad8ce1a75a
Yevhen K. <Johnsgroup001@gmail.com>
2025-08-29 19:40:44 +0100

    Initial commit

---
+++ js/task-5.js
@@ -0,0 +1,15 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const body = document.querySelector('body');
+const color = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+const handlerClick = event => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
+};
+changeColorBtn.addEventListener('click', handlerClick);
